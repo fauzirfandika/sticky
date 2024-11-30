40ac6229fb7da1c6446dbf497353ce846dfbe3dc
Fauzi Irfandika <fauzirfandika@gmail.com>
2024-11-30 16:19:46 +0700

    first prototype

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,10 +1,21 @@
 var modal = document.getElementById("cover");
-var btn = document.getElementById("mybutt");
-var mus = document.getElementById("audio");
+var mdlopen = document.getElementById("modal-open");
+var btnstop = document.getElementById("audio-stop");
+var bgaud = document.getElementById("audio");
 
-btn.onclick = function() {
+function togglePlay() {
+  return bgaud.paused ? bgaud.play() : bgaud.pause();
+};
+
+mdlopen.onclick = function() {
   modal.style.top = "-110lvh";
   document.getElementById("hero").classList.remove("fixed");
   document.getElementById("content").classList.remove("fixed");
-  mus.play()
+  bgaud.play()
 }
+
+btnstop.onclick = function () {
+  togglePlay()
+}
+
+
