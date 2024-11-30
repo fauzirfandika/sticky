c6ea559afc93543c70ac87351960bda02fd1fb7e
Fauzi Irfandika <fauzirfandika@gmail.com>
2024-12-01 03:58:44 +0700

    test cloudflare wrangler

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
+
 // https://astro.build/config
 export default defineConfig({
   output:'server',
   adapter: cloudflare({
-    imageService:'cloudflare'
+    imageService:'cloudflare',
+    platformProxy: {
+      enabled: true,
+      configPath: 'wrangler.toml',
+    },
   })
 });
+
