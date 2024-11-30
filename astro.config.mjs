17e8292625ac07802525b6419d3a3e81e1bf73cd
Fauzi Irfandika <fauzirfandika@gmail.com>
2024-11-30 14:31:09 +0700

    fix image service for cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
   output:'server',
-  adapter: cloudflare()
+  adapter: cloudflare({
+    imageService:'cloudflare'
+  })
 });
