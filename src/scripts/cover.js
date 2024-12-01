7a8014c4bf7453cf29fb83d82286034acbaf7793
Fauzi Irfandika <fauzirfandika@gmail.com>
2024-12-01 14:37:48 +0700

    merapihkan

--- src/scripts/cover.js
+++
@@ -1,9 +0,0 @@
-function myCover() {
-    let scrollPos = window.scrollY;
-    if (scrollPos >= 100) {
-        document.getElementById("under").className = "foc";
-    }
-    else () => {
-        document.getElementById("under").className = "";
-    }
-};
