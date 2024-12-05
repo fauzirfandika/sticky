a16edd040d6dc155847186072da14fa3c5adbc52
Fauzi Irfandika <fauzirfandika@gmail.com>
2024-12-05 22:55:34 +0700

    fix wrong method

--- src/scripts/fecth.js
+++ src/scripts/fecth.js
@@ -1,6 +1,6 @@
 const response = await fetch("https://app.nocodb.com/api/v2/tables/m7w29ys11yjrtsv/records",
   {
-    method: 'POST',
+    method: 'GET',
     headers: {
     'xc-token': 'RzPMz79xLsGvt0YBpZ-XnQ0YGIBeFtzx10oW-rvT'
     },
