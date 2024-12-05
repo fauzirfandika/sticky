e489fe6584fc7734783fe7001b819bebd183aaae
Fauzi Irfandika <fauzirfandika@gmail.com>
2024-12-05 14:00:46 +0700

    locals on enc.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,3 +2,8 @@
 /// <reference types="astro/client" />
 /// <reference path="worker-configuration.d.ts" />
 
+type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
+
+declare namespace App {
+	interface Locals extends Runtime {}
+}
