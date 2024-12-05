53122d83ef7c676ebfe0d9c4d352a4e1adf16b84
Fauzi Irfandika <fauzirfandika@gmail.com>
2024-12-05 13:51:20 +0700

    test env

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,8 +2,3 @@
 /// <reference types="astro/client" />
 /// <reference path="worker-configuration.d.ts" />
 
-export function GET(context) {
-  const runtime = context.locals.runtime;
-
-  return new Response('Test');
-}
