21e4074730f826c3eb73d62349eec05c165ee4ba
Fauzi Irfandika <fauzirfandika@gmail.com>
2024-12-11 02:16:49 +0700

    improve ucapan dan doa

--- src/pages/api/noco.json.ts
+++ src/pages/api/noco.json.ts
@@ -1,7 +1,7 @@
 import type { APIRoute } from "astro";
 
 export const GET: APIRoute = async (context) => {
-  const res = await fetch ("https://app.nocodb.com/api/v2/tables/mj9yvft5vib78kp/records?limit=7&fields=nama,comment,nama_init&sort=-Id&where=(comment_check_auto,eq,true)", {
+  const res = await fetch ("https://app.nocodb.com/api/v2/tables/mj9yvft5vib78kp/records?limit=25&fields=nama,comment,nama_init,rsvp&sort=-Id&where=(comment_check_auto,eq,true)", {
     method: "GET",
     headers: {'xc-token': 'RzPMz79xLsGvt0YBpZ-XnQ0YGIBeFtzx10oW-rvT'}
   });
