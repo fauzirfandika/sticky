8253da5b43410281acad6babda60f2806aa4ed65
Fauzi Irfandika <fauzirfandika@gmail.com>
2024-12-11 02:43:28 +0700

    remove test from page, add runtime to nocoPrivate.json

--- src/pages/api/nocoPrivate.json.ts
+++ src/pages/api/nocoPrivate.json.ts
@@ -1,4 +1,9 @@
 import type { APIRoute } from "astro";
+type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
+
+declare namespace App {
+	interface Locals extends Runtime {}
+};
 
 export const GET: APIRoute = async (context) => {
   const res = await fetch (`${Astro.locals.runtime.env.NOCO_URL}?fields=nama,rsvp,comment&sort=-Id`, {
