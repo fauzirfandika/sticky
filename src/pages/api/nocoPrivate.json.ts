05393a9e6eab600fa5761bc39868409c9bd9caf7
Fauzi Irfandika <fauzirfandika@gmail.com>
2024-12-11 02:34:31 +0700

    test env, misstype runetime

--- src/pages/api/nocoPrivate.json.ts
+++ src/pages/api/nocoPrivate.json.ts
@@ -1,9 +1,9 @@
 import type { APIRoute } from "astro";
 
 export const GET: APIRoute = async (context) => {
-  const res = await fetch (`${Astro.locals.runetime.env.NOCO_URL}?fields=nama,rsvp,comment&sort=-Id`, {
+  const res = await fetch (`${Astro.locals.runtime.env.NOCO_URL}?fields=nama,rsvp,comment&sort=-Id`, {
     method: "GET",
-    headers: {'xc-token': `${Astro.locals.runetime.env.NOCO_TOK}`}
+    headers: {'xc-token': `${Astro.locals.runtime.env.NOCO_TOK}`}
   });
   const data = await res.json();
   return new Response(JSON.stringify(data));
